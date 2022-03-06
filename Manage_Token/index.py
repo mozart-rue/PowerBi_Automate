bfb69c460b21a31146d2b8c40eec4880e9be397f
mozart-rue <mozartrue@outlook.com>
2022-03-06 15:49:07 -0300

    feat: Criado verificação de validade do token

--- Manage_Token/index.py
+++ Manage_Token/index.py
@@ -24,6 +24,23 @@ def postToken():
 # Faz a consulta ao banco de dados e retorna 
 def getToken():
     query = db.collection('API').document('token').get()
-    query.to_dict()
+    query = query.to_dict()
     return query
 
+
+# Gerencia se vai criar novo token ou usar existente
+def Token_Manager():
+    # Recuperar do banco hora que expira a chave
+    api_return = getToken()
+    expires_on = api_return["expires_on"]
+
+    # Recupera hora para verificação
+    local_time = time.strftime('%Y/%m/%d %H:%M:%S', time.localtime())
+
+    # Verifica se o token está expirado
+    if expires_on > local_time:
+        print('O Token está valido')
+    else:
+        print('Precisa gerar um novo token')
+
+Token_Manager()
