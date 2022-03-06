9b32125a85aaee6b807da2ec667c536d2110c74b
mozart-rue <mozartrue@outlook.com>
2022-03-06 10:00:37 -0300

    feat: Criado string de tokens

--- Manage_Token/getToken.py
+++ Manage_Token/getToken.py
@@ -1,6 +1,5 @@
 # Importando Bibliotecas
 import json
-import pandas as pd
 import time
 
 # Importando arquivos
@@ -12,8 +11,17 @@ data = json.loads(main.response.text)
 # Desconstruindo o retorno para uso das informações
 api_token = data["access_token"]
 token_type = data["token_type"]
+refresh_token = data["refresh_token"]
+token_id = data["id_token"]
 
-# Recuperando o token
-print(token_type)
-print(api_token)
+created_at = time.strftime('%Y/%m/%d %H:%M:%S', time.localtime())
+
+# Criando objeto com as informações retornadas da API
+tokens = {
+        "access_token": f"{api_token}",
+        "token_type": f"{token_type}",
+        "refresh_token": f"{refresh_token}",
+        "id_token": f"{token_id}",
+        "created_at": f"{created_at}"
+        }
 
