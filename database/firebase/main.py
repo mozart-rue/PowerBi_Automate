4509ece4f5ebb7d85826e7e056b55e3d302308ef
mozart-rue <mozartrue@outlook.com>
2022-03-06 10:32:04 -0300

    feat: Criada conexão com firestore

--- database/firebase/main.py
+++ database/firebase/main.py
@@ -1,6 +1,8 @@
 import firebase_admin
 from firebase_admin import credentials
+from firebase_admin import firestore
 
 cred = credentials.Certificate("./config/serviceAccountKey.json")
 firebase_admin.initiliaze_app(cred)
 
+db = firestore.client()
