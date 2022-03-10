9fcb515b36454229f893ae94d842096edd4c769b
mozart-rue <mozartrue@outlook.com>
2022-03-09 21:30:49 -0300

    feat: Tratamento do retorno da API em dataframe

--- app/controllers/getDatasets.py
+++ app/controllers/getDatasets.py
@@ -26,14 +26,15 @@ sys.path.append(os.path.abspath(f"{path}"))
 
 # Importando bibliotecas python
 import requests
+import pandas as pd
 
-# Importando arquivo com função token
+# Importando arquivos
 import Manage_Token.index as MT
 
 # Recuperando dados de acesso
 access_object = MT.Token_Manager()
 
-# Definir parametros para consumir a API 
+# Definindo parametros para consumir a API 
 url = 'https://api.powerbi.com/v1.0/myorg/datasets'
 token_type = access_object["token_type"]
 token = access_object["access_token"]
@@ -42,7 +43,24 @@ header = {
         "Authorization": f"{token_type} {token}"
         }
 
-response = requests.request("GET", url, headers=header )
 
-print(response.text)
+# Função que faz o Request a API de DATASETS e faz o tratamento da resposta
+def GET_Datasets():
+    # Fazendo o Request na API
+    response = requests.request("GET", url, headers=header )
 
+    # Quebrando o JSON de retorno
+    json_data = response.json()
+    data = json_data["value"]
+
+    # Definindo o dataframe
+    response_df = pd.DataFrame()
+
+    # Inserindo os dados da API no Dataframe
+    res_itens = pd.DataFrame.from_records(data)
+    response_df = response_df.append(res_itens, ignore_index=True)
+
+    # Filtrando os dados do Dataframe
+    response_df = response_df[["id", "name", "webUrl", "isRefreshable", "isOnPremGatewayRequired"]]
+
+    print(response_df)
