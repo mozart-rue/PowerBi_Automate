6b4bf80e4cc632de89fdf9e97e4594eb7422d00f
mozart-rue <mozartrue@outlook.com>
2022-03-11 20:34:51 -0300

    feat: Consumindo API's GET Datasource

--- app/controllers/datasourcesController.py
+++ app/controllers/datasourcesController.py
@@ -55,4 +55,40 @@ except Exception as error:
 
 
 
+# Se a teve resultado do banco de dados continua para consumir as API's
+if connected == True:
+    
+    # Definindo parametros para consumir as API's
+    access_list = tkn.Token_Manager()
+
+    token_type = access_list["token_type"]
+    token = access_list["access_token"]
+    
+    header = {
+            "Authorization": f"{token_type} {token}"
+            }
+
+    datasources_array = {}
+
+    total_itens = len(db_return)
+
+    for datasetId, name in db_return:
+        print(f'Name: {name}, id: {datasetId}')
+        url = f"https://api.powerbi.com/v1.0/myorg/datasets/{datasetId}/datasources"
+
+        datasources = requests.request("GET", url, headers= header )
+        print(f'API 1:\n{datasources.json()}\n')
+
+        values = datasources.json()
+        value = values["value"]
+
+        datasourceId = value[0]["datasourceId"]
+        gatewayId = value[0]["gatewayId"]
+        
+        print(f'Datasource ID: {datasourceId}, Gateway ID: {gatewayId}\n')
+        url_gw = f'https://api.powerbi.com/v1.0/myorg/gateways/{gatewayId}/datasources/{datasourceId}'
+
+        datasources_complete = requests.request("GET", url_gw, headers= header )
+
+        print(f'API 2: \n{datasources_complete.json()}\n')
 
