ef00bb0c6cf7694ed71bd2bfa30e095a1e5b0f1b
mozart-rue <mozartrue@outlook.com>
2022-03-12 11:37:07 -0300

    feat: Criado arquivo model de insercao na tabela gateways

---
+++ app/models/gatewayStatusModel.py
@@ -0,0 +1,23 @@
+# ----------------------- ADD RAIZ DO PROJETO AO ARQUIVO  ----------------------- #
+# Adicionando o path para a Raiz do projeto dinamicamente para fazer a importação de arquivos.py
+import sys
+import os
+
+def Resolve_Path():
+    # Recupera o diretorio atual e separa o ultimo iten
+    current_path = os.getcwd() # --> retorna diretorio atual: ex: (main/path/to/folder)
+    split_path = os.path.split(current_path) # --> retorna: (main/path/to, folder)
+    head, tail = split_path # --> recupera os dois itens um para cada variavel (em ordem)
+
+    # Encontra o path até a raiz do projeto (raiz = InfoCargas)
+    while not head.endswith('InfoCargas'):
+        split_path = os.path.split(head)
+        head, tail = split_path
+
+    return head
+
+path = Resolve_Path()
+sys.path.append(os.path.abspath(f"{path}"))
+# ---------------------------------------------------------------------------------- #
+
+
