c74bea2234c2e511a7183f1fe0a4467ee1166f4a
mozart-rue <mozartrue@outlook.com>
2022-03-12 15:29:10 -0300

    feat: tratativa na reposta da API para inserir no banco

--- app/controllers/refreshHistoryController.py
+++ app/controllers/refreshHistoryController.py
@@ -25,6 +25,7 @@ sys.path.append(os.path.abspath(f"{path}"))
 # ---------------------------------------------------------------------------------- #
 
 import requests
+from datetime import datetime
 
 import database.postgres.main as database
 import Manage_Token.index as tkn
@@ -72,9 +73,39 @@ if connected == True:
         res = response.json()
         data = res["value"]
 
-        print(data)
+        # Se a lista não estiver vazia continua para inserir
+        if data:
+        
+            for i in data:
 
-    
+                # Recupera valores para inserir no banco
+                refresh_type = i["refreshType"]
+                status = i["status"]
+                start_time = i["startTime"]
+                end_time = i["endTime"]
+                request_id = i["requestId"]
+                history_id = i["id"]
+
+                created_at = datetime.now()
+
+                # Criando dict para inserir no banco
+                insert = {
+                        "dataset_name": f"{name}",
+                        "dataset_id": f"{datasetId}",
+                        "status": f"{status}",
+                        "refresh_type": f"{refresh_type}",
+                        "start_time": f"{start_time}",
+                        "end_time": f"{end_time}",
+                        "request_id": f"{request_id}",
+                        "history_id": f"{history_id}",
+                        "inserted_at": f"{created_at}"
+                        }
+
+                print(f'{insert}\n')
+                        
+        else:
+            print(f"{datasetId} Name: {name} não posui historico de atualizações ainda")
+
 else:
     print('getRefreshHistoryController :: Erro => Não foi possivel executar a query no banco de dados')
 
