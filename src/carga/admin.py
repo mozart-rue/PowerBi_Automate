4563c972ab35d53ea2ddf4116e31f57b1f917a7a
mozart-rue <mozartrue@outlook.com>
2022-03-21 22:40:12 -0300

    feat: Adicionado models a pagina admin

--- src/carga/admin.py
+++ src/carga/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Datasets, Datasources, Gateways, RefreshHistory, ScheduledRefresh
 
 # Register your models here.
+admin.site.register(Datasets)
+admin.site.register(Datasources)
+admin.site.register(Gateways)
+admin.site.register(RefreshHistory)
+admin.site.register(ScheduledRefresh)
