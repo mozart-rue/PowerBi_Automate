1884595208413fcc5fd519ea778b259df78c98e4
mozart-rue <mozartrue@outlook.com>
2022-03-21 22:46:58 -0300

    feat: Configurado views e view teste

--- src/carga/views.py
+++ src/carga/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
+
+from .models import Datasets
 
 # Create your views here.
+
+# view homepage
+class Homepage(generic.ListView):
+    model = Datasets
+    template_name = "homeCargas.html"
+
+class hometeste(generic.TemplateView):
+    template_name = "base.html"
