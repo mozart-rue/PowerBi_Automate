8f38b98c212fab5daf461e77610d2cb7c093928d
mozart-rue <mozartrue@outlook.com>
2022-03-23 23:34:46 -0300

    feat: refresh_history model conf. para homepage

--- src/carga/views.py
+++ src/carga/views.py
@@ -6,6 +6,19 @@ from .models import Datasets, RefreshHistory
 # Create your views here.
 
 # view homepage
-class Homepage(generic.DetailView):
+class Homepage(generic.ListView):
     model = RefreshHistory
     template_name = "homepage.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super(Homepage, self).get_context_data(**kwargs)
+        # Filtrar a tabela de filmes pegando os filmes com categoria é igual a categoria do filme da pagina
+        historicoDetalhado = RefreshHistory.objects.distinct('dataset_name')
+        context["historicoDetalhado"] = historicoDetalhado
+
+        failedRefresh = RefreshHistory.objects.filter(status = 'Failed').count()
+        completeRefresh = RefreshHistory.objects.filter(status = 'Completed').count()
+        context["failedRefresh"] = failedRefresh
+        context["completeRefresh"] = completeRefresh
+
+        return context
