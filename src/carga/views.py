89c97b2ec12c4691cebffe7a46521d3a1c26119e
mozart-rue <mozartrue@outlook.com>
2022-03-23 21:48:34 -0300

    feat: Alterado modelo e tipo de view para detail

--- src/carga/views.py
+++ src/carga/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.views import generic
 
-from .models import Datasets
+from .models import Datasets, RefreshHistory
 
 # Create your views here.
 
 # view homepage
-class Homepage(generic.ListView):
-    model = Datasets
+class Homepage(generic.DetailView):
+    model = RefreshHistory
     template_name = "homepage.html"
