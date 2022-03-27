9a15b3ab061e9c0cb9ab268de025083da2f867a8
mozart-rue <mozartrue@outlook.com>
2022-03-27 11:10:44 -0300

    feat: Configurado Model da nova tabela

--- src/carga/views.py
+++ src/carga/views.py
@@ -1,24 +1,27 @@
 from django.shortcuts import render
 from django.views import generic
 
-from .models import Datasets, RefreshHistory
+from .models import LastRefresh, RefreshHistory
 
 # Create your views here.
 
 # view homepage
 class Homepage(generic.ListView):
-    model = RefreshHistory
+    model = LastRefresh
     template_name = "homepage.html"
     
     def get_context_data(self, **kwargs):
         context = super(Homepage, self).get_context_data(**kwargs)
         # Filtrar a tabela de filmes pegando os filmes com categoria é igual a categoria do filme da pagina
-        historicoDetalhado = RefreshHistory.objects.distinct('dataset_name')
-        context["historicoDetalhado"] = historicoDetalhado
+        last_refresh = LastRefresh.objects.distinct('dataset_name')
+        context["lastRefresh"] = last_refresh
 
-        failedRefresh = RefreshHistory.objects.filter(status = 'Failed').count()
-        completeRefresh = RefreshHistory.objects.filter(status = 'Completed').count()
+        failedRefresh = LastRefresh.objects.filter(status = 'Failed').count()
+        completeRefresh = LastRefresh.objects.filter(status = 'Completed').count()
+        inProgressRefresh = LastRefresh.objects.filter(status = 'InProgress').count()
+
         context["failedRefresh"] = failedRefresh
         context["completeRefresh"] = completeRefresh
+        context["inProgressRefresh"] = inProgressRefresh
 
         return context
